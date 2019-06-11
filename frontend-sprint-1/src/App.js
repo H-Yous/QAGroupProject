7a2e3c4bb4f95a476a0d9ad8515783241315dc04
fowusu <blackichan200@gmail.com>
2019-06-11 09:43:30 +0100

    Axios connection added to home

--- frontend-sprint-1/src/App.js
+++ frontend-sprint-1/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home } from './Home'; 
+import Home from './Home'; 
 import NowShowing from './NowShowing.js'; 
 import { NewReleases } from './NewReleases'; 
 import { Events } from './Events'; 
