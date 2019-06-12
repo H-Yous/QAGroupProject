f18b88d258bdcb09b216226bc9cb3ead0975a9d5
nickrstewarttds <nickrstewarttds@gmail.com>
2019-06-12 15:01:00 +0100

    added almost-working contact emailing functionality

--- frontend-sprint-1/src/App.js
+++ frontend-sprint-1/src/App.js
@@ -11,30 +11,47 @@ import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout'; 
 import { NavigationBar} from './components/NavigationBar';
 import { Jumbotron } from './components/Jumbotron'; 
+import PropTypes from 'prop-types';
+import { env } from './config';
 
 class App extends Component {
+  
   render() {
-  return (
-    <React.Fragment> 
-      <Layout>
-      <Router>
-      <NavigationBar />
-      <Jumbotron />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/nowShowing" component={NowShowing} />
-          <Route path="/newReleases" component={NewReleases} />
-          <Route path="/events" component={Events} />
-          <Route path="/screens" component={Screens} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
-      </Layout>
-    </React.Fragment> 
-  );
+
+    const ContactPage = (props) => {
+      return (
+        <Contact
+          env={this.props.env.bind(this.props.env)} 
+        />
+      );
+    }
+
+    return (
+      <React.Fragment> 
+        <Layout>
+        <Router>
+        <NavigationBar />
+        <Jumbotron />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/nowShowing" component={NowShowing} />
+            <Route path="/newReleases" component={NewReleases} />
+            <Route path="/events" component={Events} />
+            <Route path="/screens" component={Screens} />
+            <Route path="/about" component={About} />
+            <Route path = "/" component={Contact} />
+            <Route exact path="/contact" render={ContactPage} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+        </Layout>
+      </React.Fragment> 
+    );
+  }
 }
-}
+
+App.propTypes = {
+  env: PropTypes.object.isRequired
+};
 
 export default App;
