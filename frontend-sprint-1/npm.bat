769cb1a337da9a51771762d1e1cf5b1db7f7aab2
nickrstewarttds <nickrstewarttds@gmail.com>
2019-06-12 15:35:21 +0100

    added batch files for backend and frontend

---
+++ frontend-sprint-1/npm.bat
@@ -0,0 +1,16 @@
+npm i --save react-scripts
+npm i --save react-dom
+npm i --save react-native-web
+npm i --save react-art
+npm i --save react-router-native
+npm i --save react-router-dom
+npm i --save react-script-tag
+npm i --save react-table
+npm i --save bootstrap
+npm i --save reactstrap
+npm i --save react
+npm i -g serve
+npm i --save emailjs-com
+npm i --save prop-types
+npm i --save @seatsio/seatsio-react
+npm i
