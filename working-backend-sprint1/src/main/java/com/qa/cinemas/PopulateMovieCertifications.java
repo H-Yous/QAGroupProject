b1878a0a48a000d38d406e78f5cb230e93d2d40c
markmuwonge <contactmarkmuwonge@gmail.com>
2019-06-17 21:23:36 +0100

    created seperate start up classes for populating certifications, new releases, now showing & upcoming.
    constants in place for their corresponding controllers

---
+++ working-backend-sprint1/src/main/java/com/qa/cinemas/PopulateMovieCertifications.java
@@ -0,0 +1,71 @@
+package com.qa.cinemas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.qa.cinemas.domain.Certification;
+import com.qa.cinemas.repository.CertificationRepository;
+
+@Component
+public class PopulateMovieCertifications {
+	
+	private String apiURI;
+	private RestTemplate restTemplate;
+	private String returnedJsonString;
+	private JSONObject returnedJsonStringAsObj;
+	private JSONArray resultsArray;
+	
+	private List<String> movieRating;
+	private List<String> movieRatingDescription;
+	
+	private Certification certification;
+	
+	@Autowired
+	private CertificationRepository certificationRepository;
+	
+	public PopulateMovieCertifications()
+	{
+		movieRating = new ArrayList<String>();
+		movieRatingDescription = new ArrayList<String>();
+	
+	}
+	
+	public void start()
+	{
+		apiURI = "https://api.themoviedb.org/3/certification/movie/list?api_key=e527fe3aa9735362a7f95d86cd6093ad";
+		restTemplate = new RestTemplate();
+
+		returnedJsonString = restTemplate.getForObject(apiURI, String.class);
+		returnedJsonStringAsObj = new JSONObject(returnedJsonString);
+
+		JSONObject certificationsObj = returnedJsonStringAsObj.getJSONObject("certifications");
+
+		resultsArray = certificationsObj.getJSONArray("GB");
+
+		StreamSupport.stream(resultsArray.spliterator(), false).map(aCertification -> (JSONObject) aCertification)
+				.filter(aCertification -> aCertification.getString("certification").matches("U|PG|12A|15|18"))
+				.forEach(aCertification -> movieRating.add(aCertification.getString("certification")));
+
+		StreamSupport.stream(resultsArray.spliterator(), false).map(aCertification -> (JSONObject) aCertification)
+				.filter(aCertification -> aCertification.getString("certification").matches("U|PG|12A|15|18"))
+				.forEach(aCertification -> movieRatingDescription.add(aCertification.getString("meaning")));
+
+		movieRating.stream().forEach(x -> populateDBWithCertifications(movieRating.indexOf(x)));
+
+	}
+	
+	private void populateDBWithCertifications(int index) {
+		certification = new Certification();
+		certification.setName(movieRating.get(index));
+		certification.setDescription(movieRatingDescription.get(index));
+
+		certificationRepository.insert(certification);
+	}
+}
