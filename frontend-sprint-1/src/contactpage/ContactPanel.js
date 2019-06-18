58301a17664f6a2cc4328c6e805cf4a7c7f9995b
nickrstewarttds <nickrstewarttds@gmail.com>
2019-06-18 14:52:28 +0100

    fixed contacts page

--- frontend-sprint-1/src/contactpage/ContactPanel.js
+++ frontend-sprint-1/src/contactpage/ContactPanel.js
@@ -3,6 +3,7 @@ import { View } from "react-native";
 import { Link, withRouter } from "react-router-dom";
 import axios from "axios";
 import { PropTypes } from "prop-types";
+import { Table } from "react-bootstrap";
 
 class ContactPanel extends Component {
   static contextTypes = {
@@ -80,127 +81,136 @@ class ContactPanel extends Component {
     const feedback = this.state;
 
     return (
-      <div className="all">
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            justifyContent: "space-between"
-          }}
-        >
-          <div className="contactinfo-container">
-            <View style={{ flex: 1, flexDirection: "column" }}>
-              <label
-                className="enquirerEmailLabel"
-                id="enquirerEmail"
-                name="enquirerEmail"
-              >
-                <i>You can contact us directly here.</i>
-                <br />
-                <i>
-                  Alternatively, you can take a look at our{" "}
-                  <Link to="/faq">FAQ</Link>.
-                </i>
-              </label>
-
-              <br />
-
-              <label
-                className="contactinfoLabel"
-                id="contactinfo"
-                name="contactinfo"
-              >
-                <b>Enquiries Address:</b>
-                <br />
-                <i>c/o QA Cinemas, Ltd.</i>
-                <br />
-                PO Box 555-0123
-                <br />
-                Tammouth TM9 8EF
-                <br />
-                Alanshire
-                <br />
-                United Kingdom
-                <br />
-              </label>
-
-              <br />
-
-              <label
-                className="venuecontactinfoLabel"
-                id="venuecontactinfo"
-                name="venuecontactinfo"
-              >
-                <b>Venue Address:</b>
-                <br />
-                <i>3.329, International House</i>
-                <br />
-                1 St. Katharine's Way
-                <br />
-                St. Katharine's & Wapping
-                <br />
-                London E1W 1UN
-                <br />
-                Greater London
-                <br />
-                United Kingdom
-              </label>
-
-              <br />
-
-              <label
-                className="headquartersLabel"
-                id="headquartersinfo"
-                name="headquartersinfo"
-              >
-                <b>QA Cinemas International HQ:</b>
-                <br />
-                <i>Rath House</i>
-                <br />
-                55-65 Uxbridge Road
-                <br />
-                Slough SL1 1SQ
-                <br />
-                Berkshire <br />
-                United Kingdom
-              </label>
-            </View>
-          </div>
-
-          <div className="contact-container">
-            <form className="feedback" onSubmit={this.handleSubmit}>
-              <View style={{ flex: 1, flexDirection: "column", width: 750 }}>
-                <input
-                  className="text-input"
+      <Table striped bordered variant="dark" size="sm">
+        <div className="all">
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              padding: 20
+            }}
+          >
+            <div className="contactinfo-container">
+              <View style={{ flex: 1, flexDirection: "column" }}>
+                <label
+                  className="enquirerEmailLabel"
                   id="enquirerEmail"
                   name="enquirerEmail"
-                  onChange={this.handleEmailChange.bind(this)}
-                  value={this.state.enquirerEmail}
-                  placeholder={this.state.enquirerPlaceholder}
-                  required
-                />
+                >
+                  <i>You can contact us directly here.</i>
+                  <br />
+                  <i>
+                    Alternatively, you can take a look at our{" "}
+                    <Link to="/faq">FAQ</Link>.
+                  </i>
+                </label>
 
                 <br />
 
-                <textarea
-                  className="text-input"
-                  id="feedback"
-                  name="feedback"
-                  onChange={this.handleFeedbackChange.bind(this)}
-                  value={this.state.feedback}
-                  rows="20"
-                  placeholder={this.state.placeholder}
-                  required
-                />
+                <label
+                  className="contactinfoLabel"
+                  id="contactinfo"
+                  name="contactinfo"
+                >
+                  <b>Enquiries Address:</b>
+                  <br />
+                  <i>c/o QA Cinemas, Ltd.</i>
+                  <br />
+                  PO Box 555-0123
+                  <br />
+                  Tammouth TM9 8EF
+                  <br />
+                  Alanshire
+                  <br />
+                  United Kingdom
+                  <br />
+                </label>
 
                 <br />
 
-                <input type="submit" value="Submit" />
+                <label
+                  className="venuecontactinfoLabel"
+                  id="venuecontactinfo"
+                  name="venuecontactinfo"
+                >
+                  <b>Venue Address:</b>
+                  <br />
+                  <i>3.329, International House</i>
+                  <br />
+                  1 St. Katharine's Way
+                  <br />
+                  St. Katharine's & Wapping
+                  <br />
+                  London E1W 1UN
+                  <br />
+                  Greater London
+                  <br />
+                  United Kingdom
+                </label>
+
+                <br />
+
+                <label
+                  className="headquartersLabel"
+                  id="headquartersinfo"
+                  name="headquartersinfo"
+                >
+                  <b>QA Cinemas International HQ:</b>
+                  <br />
+                  <i>Rath House</i>
+                  <br />
+                  55-65 Uxbridge Road
+                  <br />
+                  Slough SL1 1SQ
+                  <br />
+                  Berkshire <br />
+                  United Kingdom
+                </label>
               </View>
-            </form>
-          </div>
-        </View>
-      </div>
+            </div>
+            <div className="contact-container">
+              <form className="feedback" onSubmit={this.handleSubmit}>
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: "column",
+                    width: 750,
+                    padding: 20
+                  }}
+                >
+                  <input
+                    className="text-input"
+                    id="enquirerEmail"
+                    name="enquirerEmail"
+                    onChange={this.handleEmailChange.bind(this)}
+                    value={this.state.enquirerEmail}
+                    placeholder={this.state.enquirerPlaceholder}
+                    style={{ backgroundColor: "lightgray" }}
+                    required
+                  />
+
+                  <textarea
+                    className="text-input"
+                    id="feedback"
+                    name="feedback"
+                    onChange={this.handleFeedbackChange.bind(this)}
+                    value={this.state.feedback}
+                    rows="20"
+                    placeholder={this.state.placeholder}
+                    required
+                    style={{
+                      backgroundColor: "lightgray"
+                    }}
+                  />
+
+                  <input type="submit" value="Submit" />
+                </View>
+              </form>
+            </div>
+          </View>
+        </div>
+      </Table>
     );
   }
 }
